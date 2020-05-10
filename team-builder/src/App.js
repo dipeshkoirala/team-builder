e1915b198459df92eb03232984c087b33910b67b
dipeshkoirala <dipeshkoirala@gmail.com>
2020-05-10 19:35:51 -0400

    index and app js with Heading

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// import logo from "./logo.svg";
+
+import "./App.css";
 
 function App() {
+  //
+
+  const [team, setTeam] = useState([
+    {
+      id: "",
+      date: "",
+      fullName: "",
+      email: "",
+      info: " ",
+    },
+  ]);
+
+  const addNewTeam = (member) => {
+    const newTeam = {
+      id: team.length + 1,
+      date: member.date,
+      fullName: member.fullName,
+      email: member.email,
+      info: member.info,
+    };
+    setTeam([...team, newTeam]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Team Build</h1>
+      
     </div>
   );
 }
